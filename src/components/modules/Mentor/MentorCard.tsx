3994ca11ff255c73e9a40d6b0cb9fce93c1f2f9d
Shahin Alam <shahinexy@gmail.com>
2025-09-05 12:50:12 +0600

    feat: enhance MentorCard component with pagination and update user limit to 16

--- src/components/modules/Mentor/MentorCard.tsx
+++ src/components/modules/Mentor/MentorCard.tsx
@@ -5,10 +5,14 @@ import user from "../../../assets/placeholders/user-placeholder.jpg";
 import Spinner from "@/components/common/Spinner";
 import { useAllUserQuery } from "@/redux/features/users/users.api";
 import Link from "next/link";
+import Pagination from "@/components/common/Pagination";
+import { useState } from "react";
 
 const MentorCard = () => {
+  const [currentPage, setCurrentPage] = useState(1);
   const { data, isFetching } = useAllUserQuery([
-    { name: "limit", value: 12 },
+    { name: "limit", value: 16 },
+    { name: "page", value: String(currentPage) },
     { name: "searchTerm", value: "MENTOR" },
   ]);
 
@@ -17,25 +21,38 @@ const MentorCard = () => {
   if (isFetching) {
     return <Spinner />;
   }
-  return (
-    <div className="grid md:grid-cols-4 gap-6">
-      {datas?.map((item: any) => (
-        <Link
-          href={`/mentor/${item.id}`}
-          key={item?.id}
-          className="border border-white/50 p-3 rounded-lg space-y-3"
-        >
-          <Image
-            src={item?.profileImage || user}
-            alt="user"
-            width={300}
-            height={200}
-            className="w-full h-48 rounded-lg "
-          />
 
-          <p>{item.fullName}</p>
-        </Link>
-      ))}
+  const metaData = data?.data?.meta;
+  return (
+    <div className="space-y-7">
+      <div className="grid md:grid-cols-4 gap-6">
+        {datas?.map((item: any) => (
+          <Link
+            href={`/mentor/${item.id}`}
+            key={item?.id}
+            className="border border-white/50 p-3 rounded-lg space-y-3"
+          >
+            <Image
+              src={item?.profileImage || user}
+              alt="user"
+              width={300}
+              height={200}
+              className="w-full h-48 rounded-lg "
+            />
+
+            <p>{item.fullName}</p>
+          </Link>
+        ))}
+      </div>
+
+      {metaData?.total > 16 && (
+        <Pagination
+          currentPage={metaData?.page}
+          totalItem={metaData?.total}
+          limit={16}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      )}
     </div>
   );
 };
